9716e0d2c1a4be8af4b77b78ff5447ac904b3e89
Evans Maina <marsappollo3@gmail.com>
2025-06-08 17:46:13 +0300

    Remove AuthPage component from authentication module to streamline codebase and eliminate unused files.

--- src/app/(auth)/auth/[[...auth]]/page.tsx
+++ src/app/(auth)/auth/[[...auth]]/page.tsx
@@ -1,5 +1,5 @@
 import { Suspense } from "react";
-import { SignInButtons } from "./sign-in-buttons";
+import { SignInButtons } from "../sign-in-buttons";
 
 export default function AuthPage() {
   return (
